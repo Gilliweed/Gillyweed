09b58671a355434e9a2ba673370f664af8516416
Mohityadav94161 <mohityadav94161@gmail.com>
2021-12-29 19:54:32 +0530

    pull

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -4,18 +4,23 @@ import Home from './ShopPages/Home';
 import Cart from '../Shop/ShopPages/Cart';
 import Navbar from './ShopComponents/Navbar';
 import Footer from './ShopComponents/Footer';
-import MoreProductList from './ShopComponents/Products'
+import MoreProductList from './ShopComponents/Products';
 import NewsLetter from '../../components/NewsLetter';
+import {Example} from './ShopComponents/HomePage/FlashDeal/FlashDealCard';
 const Shop = () => {
   return (
     <div style={{}}>
-      <Navbar/>
+      <Navbar />
       <div className="" style={{}}>
         <Home/>
         {/* <MoreProductList /> */}
+        <div className='bg-blue'>
+          here it is imported
+          <Example />
+        </div>
       </div>
-      <NewsLetter/>
-      <Footer/>
+      <NewsLetter />
+      {/* <Footer /> */}
     </div>
   );
 };
