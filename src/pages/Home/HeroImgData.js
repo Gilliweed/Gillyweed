96c7f1b04aee47329700a7849d141d32f0a662f5
Ayush <ayushbansal095@gmail.com>
2022-01-03 13:59:43 +0530

    Changes in the Home Page

--- src/pages/Home/HeroImgData.js
+++ src/pages/Home/HeroImgData.js
@@ -1,27 +1,31 @@
-const Data=[{
-    img: "https://images.unsplash.com/photo-1611918126831-0a8352d6196f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-    title:"Generate Lorem Ipsum placeholder text.",
-    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    stext:"Nisi lacus sed viverra tellus in hac.",    
-},
-    {img: "https://images.unsplash.com/photo-1620052581237-5d36667be337?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-    title:"Lacinia at quis risus sed vulputate odio ut.", 
-    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-     stext:"Eget aliquet nibh praesent tristique magna. Convallis convallis tellus id interdum velit laoreet.",
-},
-    {img:"https://images.unsplash.com/photo-1617114571010-c3a67a1ee5d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-    title:"Lorem ipsum dolor sit amet.",   
-    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    stext:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-
-},
-    {img:"https://images.unsplash.com/photo-1620050538592-9db01a2b7dd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-    title:"In est ante in nibh mauris cursus mattis molestie",
-    text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    stext:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo odio aenean sed adipiscing",
-
-}
-       ]
-
+const Data = [
+  {
+    img: "https://images.unsplash.com/photo-1623248459937-5af108850965?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80",
+    title: "Gillyweed",
+    text: `To safeguard the society we must protect the environment ${"\n"}Join us in the journey to fight climate change and avoid a global disaster.`,
+    stext: "Nisi lacus sed viverra tellus in hac.",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1620052581237-5d36667be337?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    title: "Is CARBON NEUTRAL a smart strategy?",
+    text: "Carbon Neutral can be accomplished by balancing the carbon dioxide emissions with their eviction or by completely reducing the emissions to zero. To avoid natural disaster, we must achieve carbon neutrality.",
+    stext:
+      "Eget aliquet nibh praesent tristique magna. Convallis convallis tellus id interdum velit laoreet.",
+  },
+  //   {
+  //     img: "https://images.unsplash.com/photo-1617114571010-c3a67a1ee5d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+  //     title: "Lorem ipsum dolor sit amet.",
+  //     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //     stext:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //   },
+  //   {
+  //     img: "https://images.unsplash.com/photo-1620050538592-9db01a2b7dd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+  //     title: "In est ante in nibh mauris cursus mattis molestie",
+  //     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //     stext:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo odio aenean sed adipiscing",
+  //   },
+];
 
 export default Data;
